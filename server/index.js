99840d0d06a0b8d0d71e9e5101b3f06849e048f7
Ethan Feldman <ethan@compass.com>
2020-04-11 12:43:34 -0400

    update message again

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
 
         if(error) return callback(error);
 
-        socket.emit('message', { user: 'Admin', text: `${user.name}, welcome to the room ${user.room}.` });
+        socket.emit('message', { user: 'Admin', text: `Hi ${user.name}. Welcome to the room ${user.room}.` });
         socket.broadcast.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has joined!` });
 
         socket.join(user.room);
